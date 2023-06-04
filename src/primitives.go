3366ce0a0eb283c37d0659e8ee6d9f7b4306533f
Abhishek Gupta <abhishekgupta1992@outlook.com>
2023-06-04 18:25:04 +0530

    Go routines and channels

---
+++ src/primitives.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+
+func someFunc(num string){
+    fmt.Println(num)
+}
+
+func main(){
+
+    go someFunc("1")
+    go someFunc("2")
+    go someFunc("3")
+
+    time.Sleep(time.Second * 2)
+
+    fmt.Println("hi")
+}
