f15fc062eefdd9b075fc5b84286ff2eeed5c3895
Abhishek Gupta <abhishekgupta1992@outlook.com>
2023-06-05 10:51:32 +0530

    added example for select statement

---
+++ src/selectDemo2.go
@@ -0,0 +1,18 @@
+package main
+
+import ("fmt"
+    "time")
+
+func main(){
+
+   go func(){
+        for{
+                select {
+                    default:
+                        fmt.Println("Doing Work")
+                }
+        }
+   }()
+
+    time.Sleep(time.Hour * 365)
+}
