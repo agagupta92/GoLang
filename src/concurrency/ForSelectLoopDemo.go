1713ce36077584e671cbc54632ce904f6f9b33ac
Abhishek Gupta <abhishekgupta1992@outlook.com>
2023-06-11 18:59:36 +0530

    Create ForSelectLoopDemo.go

---
+++ src/concurrency/ForSelectLoopDemo.go
@@ -0,0 +1,24 @@
+//For select loop demo
+
+package main
+
+import "fmt"
+
+func main() {
+
+	charChannel := make(chan string, 3)
+
+	chars := []string{"a", "b", "c"}
+
+	for _, s := range chars {
+		select {
+		case charChannel <- s:
+		}
+	}
+
+	close(charChannel)
+
+	for result := range charChannel {
+		fmt.Println(result)
+	}
+}
